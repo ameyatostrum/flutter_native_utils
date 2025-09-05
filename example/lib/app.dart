7b56364f9d071e65d036d3ea595478740ab4c550
ameyatostrum <149650298+ameyatostrum@users.noreply.github.com>
2025-09-05 07:18:16 +0530

    change ui to fluent

--- example/lib/app.dart
+++ example/lib/app.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+import 'package:fluent_ui/fluent_ui.dart';
 import 'package:flutter_native_utils/flutter_native_utils.dart';
 import 'package:flutter_native_utils_example/widgets/widgets.dart';
 
@@ -12,6 +12,9 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   final _flutterNativeUtilsPlugin = FlutterNativeUtils();
 
+  int topIndex = 1;
+  PaneDisplayMode displayMode = PaneDisplayMode.auto;
+
   @override
   void initState() {
     super.initState();
@@ -19,21 +22,41 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Flutter Native Utils'),
-        ),
-        body: Center(
-          child: Column(
-            children: [
-              TestRebootWidget(
-                onPressed: () async =>
-                    await _flutterNativeUtilsPlugin.requestAppRestart(),
+    return FluentApp(
+      home: NavigationView(
+        appBar: const NavigationAppBar(title: Text('Flutter Native Utils')),
+        pane: NavigationPane(
+            selected: topIndex,
+            onItemPressed: (index) {
+              if (index == topIndex) {
+                if (displayMode == PaneDisplayMode.open) {
+                  setState(() => displayMode = PaneDisplayMode.compact);
+                } else if (displayMode == PaneDisplayMode.compact) {
+                  setState(() => displayMode = PaneDisplayMode.open);
+                }
+              }
+            },
+            displayMode: displayMode,
+            items: [
+              PaneItem(
+                icon: const Icon(FluentIcons.home),
+                title: const Text('Home'),
+                body: TestRebootWidget(
+                  onPressed: () async =>
+                      await _flutterNativeUtilsPlugin.requestAppRestart(),
+                ),
               ),
-            ],
-          ),
-        ),
+            ]),
+        // body: Center(
+        //   child: Column(
+        //     children: [
+        //       TestRebootWidget(
+        //         onPressed: () async =>
+        //             await _flutterNativeUtilsPlugin.requestAppRestart(),
+        //       ),
+        //     ],
+        //   ),
+        // ),
       ),
     );
   }
--- example/lib/widgets/test_reboot_widget.dart
+++ example/lib/widgets/test_reboot_widget.dart
@@ -1,4 +1,5 @@
-import 'package:flutter/material.dart';
+import 'package:fluent_ui/fluent_ui.dart';
+// import 'package:flutter/material.dart';
 
 // ignore: must_be_immutable
 class TestRebootWidget extends StatelessWidget {
@@ -17,12 +18,12 @@ class TestRebootWidget extends StatelessWidget {
           padding: const EdgeInsets.symmetric(vertical: 8.0),
           child: Text(
             "Following button will reboot the application.",
-            style: Theme.of(context).textTheme.headlineSmall,
+            style: FluentTheme.of(context).typography.title,
           ),
         ),
         Padding(
           padding: const EdgeInsets.symmetric(vertical: 8.0),
-          child: ElevatedButton(
+          child: FilledButton(
             onPressed: onPressed == null
                 ? null
                 : () async => await showConfirmationDialog(
@@ -42,12 +43,12 @@ class TestRebootWidget extends StatelessWidget {
   }) async {
     await showDialog(
       context: context,
-      builder: (context) => AlertDialog(
+      builder: (context) => ContentDialog(
         title: const Text("Reboot the app"),
         content: const Text("Are you sure you want to reboot the application?"),
         actions: [
-          TextButton(onPressed: onPressed, child: const Text("Reboot")),
-          TextButton(
+          FilledButton(onPressed: onPressed, child: const Text("Reboot")),
+          Button(
               onPressed: () => Navigator.of(context).pop(),
               child: const Text("Cancel")),
         ],
